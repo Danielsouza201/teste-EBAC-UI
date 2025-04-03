1747dea4069ea3ff45e69f3e8d09620eabd1cd1a
Daniel Souza <danielsol815@gmail.com>
2025-04-03 17:11:40 -0300

    teste-função de compra de item

---
+++ cypress/e2e/3-loja-ebac/comprar.cy.js
@@ -0,0 +1,32 @@
+///<reference types="cypress"/>
+
+describe('funcionalidade: comprar item do carrinho', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('deve selecionar um item da lista', () => {
+        cy.get('#content')
+          .contains('Abominable Hoodie')
+          .click()
+          cy.get('.button-variable-item-M')
+          .click()
+          cy.get('.button-variable-item-Green')
+          .click()
+          cy.get('.single_add_to_cart_button')
+          .click()
+          cy.get('.woocommerce-message > .button')
+          .click()
+          cy.get('.wc-proceed-to-checkout')
+          .click()
+
+
+
+
+
+    
+    });
+
+    
+});
