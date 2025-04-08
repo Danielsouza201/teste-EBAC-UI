26ce524ae07e0c3001d6520b22ef036f82adf9a9
Daniel Souza <danielsol815@gmail.com>
2025-04-08 17:41:10 -0300

    aula - Page Objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,20 +1,28 @@
 ///<reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('funcionalidade - produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+       produtosPage.visitarUrl()
     });
 
     it('deve selecionar um prpduto da lista', () => {
-        cy.get(' .product-block')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Argus All-Weather Tank')
-            .click()
+        produtosPage.buscarProdutosLista('Aether Gym Pant')
+        cy.get('#tab-title-description > a').should('contain' , 'Descrição')       
+    });
 
-            cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+    it.only('deve buscar um produto com sucesso', () => {
+        let produto = 'Aero Daily Fitness Tee'
+        produtosPage.buscarProdutos(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('deve visitar a pagina de um produto', () => {
+        
+    });
+
+    it('deve adicionar um produto ao carrinho', () => {
         
     });
 });
