6c0b3ce3e6090580aa8dae9f39a13c144f19f9f6
Daniel Souza <danielsol815@gmail.com>
2025-04-29 17:15:01 -0300

    correção exercicio e2e modulo 12

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -16,54 +16,48 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   });
 
   it('selecionar o produto da lista', () => {
-    produtosPage.buscarProduto('Arcadio Gym Short')
-    produtosPage.adicionarProdutoCarrinho()
-    cy.get('.woocommerce-message').should('contain', 'foi adicionado no seu carrinho.')
-  });
+      produtosPage.buscarProduto('Arcadio Gym Short')
+      produtosPage.adicionarProdutoCarrinho()
+      cy.get('.woocommerce-message').should('contain', 'foi adicionado no seu carrinho.')
 
-  it('inserir nome e adicionar produto ao carrinho usando parametros', () =>{
-    produtosPage.pesquisarProduto('Teton Pullover Hoodie')
-    produtosPage.adicionarProdutoaoCarrinhoParametros('L', 'Black', 1)
-    cy.get('.woocommerce-message').should('contain', 'foi adicionado no seu carrinho.')
-  });
+      produtosPage.pesquisarProduto('Teton Pullover Hoodie')
+      produtosPage.adicionarProdutoaoCarrinhoParametros('L', 'Black', 1)
+      cy.get('.woocommerce-message').should('contain', 'foi adicionado no seu carrinho.')
 
-  it('deve adicionar o produto ao carrinho usando massa de dados', () =>{
-    cy.fixture('produtos').then(dados => {
+      cy.get('.logo-in-theme > .logo > a > .logo-img').click()
+      cy.fixture('produtos').then(dados => {
       produtosPage.buscarProduto(dados[0].nomeProduto)
       produtosPage.adicionarProdutoaoCarrinhoParametros(
       dados[0].tamanho,
       dados[0].cor,
       dados[0].quantidade)
       cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
-    });  
-  });
 
-  it('adicionar produto ao carrinho', () => {
-    cy.fixture('produtos').then(dados => {
+      cy.get('#primary-menu > .menu-item-629 > a').click()
+      cy.fixture('produtos').then(dados => {
       produtosPage.buscarProduto(dados[1].nomeProduto)
       produtosPage.adicionarProdutoaoCarrinhoParametros(
       dados[1].tamanho,
       dados[1].cor,
       dados[1].quantidade)
       cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+
+      produtosPage.pesquisarProduto('Teton Pullover Hoodie')
+      produtosPage.adicionarProdutoaoCarrinhoParametros('L', 'Black', 1)
+      cy.get('.woocommerce-message > .button').click()
+      cy.get('.checkout-button').click()
+      cy.get('#billing_first_name').type(faker.person.firstName('male'))
+      cy.get('#billing_last_name').type(faker.person.lastName('male'))
+      cy.get('#billing_address_1').type(faker.location.streetAddress())
+      cy.get('#billing_city').type(faker.location.city())
+      cy.get('#billing_postcode').type(faker.location.zipCode('#####-###'))
+      cy.get('#billing_phone').type(faker.phone.number('+55(##)####-####'))
+      cy.get('#billing_email').type(faker.internet.email())
+      cy.get('#terms').click()
+      cy.get('#place_order').click()
+      cy.wait(5000)
+      cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
     }); 
   });
-
-  it.only('deve preencher os dados de checkout e concluir a compra ', () => {
-    produtosPage.pesquisarProduto('Teton Pullover Hoodie')
-    produtosPage.adicionarProdutoaoCarrinhoParametros('L', 'Black', 1)
-    cy.get('.woocommerce-message > .button').click()
-    cy.get('.checkout-button').click()
-    cy.get('#billing_first_name').type(faker.person.firstName('male'))
-    cy.get('#billing_last_name').type(faker.person.lastName('male'))
-    cy.get('#billing_address_1').type(faker.location.streetAddress())
-    cy.get('#billing_city').type(faker.location.city())
-    cy.get('#billing_postcode').type(faker.location.zipCode('#####-###'))
-    cy.get('#billing_phone').type(faker.phone.number('+55(##)####-####'))
-    cy.get('#billing_email').type(faker.internet.email())
-    cy.get('#terms').click()
-    cy.get('#place_order').click()
-    cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
-  });
-
+ });
 })
